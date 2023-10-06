d66ff9a12501d9119fcfa9500e4d64e44413e047
jaclynnbarrera <jaclynnbarrera@gmail.com>
2023-10-05 22:54:52 -0400

    creates users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_27_011426) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_05_235936) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_27_011426) do
     t.string "title"
     t.integer "year"
     t.string "director"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
