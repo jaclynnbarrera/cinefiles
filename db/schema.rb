699d1a6bcb2ac4e130701ee406b0cad01b441193
jaclynnbarrera <jaclynnbarrera@gmail.com>
2023-10-10 21:10:40 -0400

    removes user model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_05_235936) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_011426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,14 +18,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_05_235936) do
     t.string "title"
     t.integer "year"
     t.string "director"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
