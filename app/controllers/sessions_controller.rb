a638eb146b46f1f50b5846848bb5c95a9e2e693e
jaclynnbarrera <jaclynnbarrera@gmail.com>
2023-10-17 18:32:52 -0400

    sets up rubocop

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,32 @@
+# frozen_string_literal: true
+
+# The SessionsController handles user authentication and login sessions.
+# It provides actions for user login, logout, and session management.
 class SessionsController < ApplicationController
-    def new
-        # render the login form
-    end
+  def new
+    # render the login form
+  end
 
-    def create
-        if admin_credentials_valid?
-            session[:admin_authenticated] = true
-            redirect_to admin_upload_path
-        else
-        flash[:error] = 'Incorrect login, please try again'
-        redirect_to login_path
-        end
-    end
+  def create
+    if admin_credentials_valid?
+      session[:admin_authenticated] = true
+      redirect_to admin_upload_path
+    else
+      flash[:error] = 'Incorrect login, please try again'
 
-    def destroy
-        session[:admin_authenticated] = nil
-        redirect_to root_path
+      redirect_to login_path
     end
+  end
 
-    private
+  def destroy
+    session[:admin_authenticated] = nil
+    redirect_to root_path
+  end
 
-    def admin_credentials_valid?
-        params.require(:session).permit(:username, :password)
-        return params[:session][:username] == ENV['ADMIN_USERNAME'] && params[:session][:password] == ENV['ADMIN_PASSWORD']
-    end
+  private
+
+  def admin_credentials_valid?
+    params.require(:session).permit(:username, :password)
+    params[:session][:username] == ENV['ADMIN_USERNAME'] && params[:session][:password] == ENV['ADMIN_PASSWORD']
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_27_011426) do
+ActiveRecord::Schema[7.0].define(version: 20_230_927_011_426) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "images", force: :cascade do |t|
-    t.string "title"
-    t.integer "year"
-    t.string "director"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'images', force: :cascade do |t|
+    t.string 'title'
+    t.integer 'year'
+    t.string 'director'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
