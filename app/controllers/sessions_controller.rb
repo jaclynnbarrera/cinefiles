7bdf21bb5fc2cfaf2eb815d7103dab5dc7c6ad18
jaclynnbarrera <jaclynnbarrera@gmail.com>
2023-10-17 14:26:46 -0400

    sets up redirect on login fail, styling to login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,24 @@ class SessionsController < ApplicationController
     end
 
     def create
-        if params[:username] == ENV['ADMIN_USERNAME'] && params[:password] == ENV['ADMIN_PASSWORD'];
-            # successful auth
+        if admin_credentials_valid?
             session[:admin_authenticated] = true
             redirect_to admin_upload_path
         else
-            # redirect not working -- why?
-            # auth failed
-            flash[:error] = 'Authentication failed'
-            render :new
+        flash[:error] = 'Incorrect login, please try again'
+        redirect_to login_path
         end
     end
 
     def destroy
         session[:admin_authenticated] = nil
         redirect_to root_path
+    end
+
+    private
+
+    def admin_credentials_valid?
+        params.require(:session).permit(:username, :password)
+        return params[:session][:username] == ENV['ADMIN_USERNAME'] && params[:session][:password] == ENV['ADMIN_PASSWORD']
     end
 end
